55ca617a31510a103bdd8060606658c4dfd6a2ff
jacob lie <jacob.l.lie97@gmail.com>
2022-03-29 10:39:49 +0200

    Thesis added

---
+++ Thesis/wordlist_sort.py
@@ -0,0 +1,5 @@
+
+
+words_and_abbriviations = ["ATM", "Cytokines", "DNA", "DNA transcription", "OD",
+                           "P53", "Phosphorylation", "RNA-primer", "ROS", "CDK"]
+print(sorted(words_and_abbriviations))
