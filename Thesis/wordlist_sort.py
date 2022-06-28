ada12990c550154aa53c9cac55b99668915302b8
jacob lie <jacob.l.lie97@gmail.com>
2022-06-28 22:44:23 +0200

    removing junk

--- Thesis/wordlist_sort.py
+++
@@ -1,5 +0,0 @@
-
-
-words_and_abbriviations = ["ATM", "Cytokines", "DNA", "DNA transcription", "OD",
-                           "P53", "Phosphorylation", "RNA-primer", "ROS", "CDK"]
-print(sorted(words_and_abbriviations))
